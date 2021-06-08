b454a5d6a36e73edac4fa84ca9e54a45387be401
Robinson Soares <soares.robinson@gmail.com>
2021-06-08 09:44:25 -0300

    Desing By Contracts

--- PaymentContext.Domain/ValueObjects/Document.cs
+++ PaymentContext.Domain/ValueObjects/Document.cs
@@ -14,5 +14,17 @@ namespace PaymentContext.Domain.ValueObjects
         public string Number { get; private set; }
         public EDocumentType Type { get; private set; }
 
+        private bool Validate()
+        {
+            if (Type == EDocumentType.CNPJ && Number.Length == 14)
+                return true;
+
+            if (Type == EDocumentType.CPF && Number.Length == 11)
+                return true;
+
+            return false;
+
+        }
+
     }
 }
